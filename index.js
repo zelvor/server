9df3b43956d89bb6512ddedf29e1001c1ed2edfd
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2022-05-30 21:55:24 +0700

    update index.js

--- index.js
+++ index.js
@@ -13,27 +13,41 @@ admin.initializeApp({
 app.use(express.json())
 
 app.post('/buyer-notifications', (req,res) => {
-  admin.firestore().collection('Users').doc(req.body.buyerId).get()
-  .then(buyerSnapshot => {
-    admin.firestore().collection('Users').doc(req.body.book.seller).get()
-    .then(sellerSnapshot => {
-        handleRegisterToBuy(buyerSnapshot.data(),sellerSnapshot.data(),req.body.book,req.body.price)
+  admin.firestore().collection('Users').doc(req.body.sender).get()
+  .then(senderSnapshot => {
+    admin.firestore().collection('Users').doc(req.body.receiver).get()
+    .then(receiverSnapshot => {
+      admin.firestore().collection('Books').doc(req.body.book).get()
+        .then(bookSnapshot => {
+          sendMessage(
+            senderSnapshot.data(),
+            receiverSnapshot.data(),
+            bookSnapshot.data(),
+            'seller',
+            req.body.type,
+            req.body.price  
+          )
+        })
       })
     })
 })
 
 app.post('/seller-notifications', (req,res) => {
-  console.log(req.body)
+  if(req.body.type == 'accepted') {
+    handleAccept(req.body.sender, req.body.receiver, req.body.book)
+  }
 })
 
-async function handleRegisterToBuy(buyerInfo, sellerInfo, book, price) {
+async function sendMessage(sender, receiver, book, kind, type, price) {
   await admin.messaging().sendToDevice(
-    sellerInfo.tokens,
+    receiver.tokens,
     {
       data: {
-        buyer: JSON.stringify(buyerInfo),
-        seller: JSON.stringify(sellerInfo),
+        sender: JSON.stringify(sender),
+        receiver: JSON.stringify(receiver),
         book: JSON.stringify(book),
+        kind: JSON.stringify(kind),
+        type: JSON.stringify(type),
         price: JSON.stringify(price)
       }
     },
@@ -45,12 +59,54 @@ async function handleRegisterToBuy(buyerInfo, sellerInfo, book, price) {
   })
 }
 
-async function handleUpdateBuyerNotification() {
-  
+async function handleAccept(seller, buyer, book) {
+  await admin.firestore().collection('Notifications').doc(seller).get()
+    .then(sellerNotiSnapshot => {
+      var notifications = sellerNotiSnapshot.data().notifications
+      for(let i = 0; i < notifications.length; i++) {
+        if(notifications[i].bookId == book) {
+          notifications.pop(notifications[i])
+        }
+      }
+      notifications.push({
+        bookId: book,
+        date: String(today.getDate()).padStart(2, '0') + '/' 
+            + String(today.getMonth() + 1).padStart(2, '0') + '/' 
+            + today.getFullYear(),
+        kind: 'seller',
+        type: 'accepted',
+        partner: buyer
+      })
+      await admin.firestore().collection('Notifications').doc(seller).update({
+        notifications: notifications
+      })
+    })
+
+  await admin.firestore().collection('Notifications').doc(seller).get()
+    .then(buyerNotiSnapshot => {
+      var notifications = buyerNotiSnapshot.data().notifications
+      for(let i = 0; i < notifications.length; i++) {
+        if(notifications[i].bookId == book) {
+          notifications.pop(notifications[i])
+        }
+      }
+      notifications.push({
+        bookId: book,
+        date: String(today.getDate()).padStart(2, '0') + '/' 
+            + String(today.getMonth() + 1).padStart(2, '0') + '/' 
+            + today.getFullYear(),
+        kind: 'buyer',
+        type: 'accepted',
+        partner: seller
+      })
+      await admin.firestore().collection('Notifications').doc(buyer).update({
+        notifications: notifications
+      })
+    })
 }
 
-async function handleUpdateSellerNotification() {
-  
+async function handleReject(seller, buyer, book) {
+  await admin.collection('Notifications').doc(seller)
 }
 
 const PORT = process.env.PORT || 3000
